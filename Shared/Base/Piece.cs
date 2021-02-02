600737532b40893be6a6d6889d3c091a5746eaba
Simon Tulling <simon.tulling99@gmail.com>
2021-02-02 09:25:58 +0100

    Add properties to pieces

--- Shared/Base/Piece.cs
+++ Shared/Base/Piece.cs
@@ -6,9 +6,9 @@ namespace ChessVariants.Shared
 {
     public abstract class Piece
     {
-        public List<Movement> movements;
-        public string name;
-        public int owner;
+        public List<Movement> movements { get; set; }
+        public string name { get; set; }
+        public int owner { get; set; }
 
         public string GetFileName()
         {
